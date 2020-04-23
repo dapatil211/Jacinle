7638a46dc06223a1871d88f92aade644883f40a0
Darshan Patil <dapatil211@gmail.com>
2020-04-23 10:18:33 +0000

    Fixed tb bug

--- jactorch/train/tb.py
+++ jactorch/train/tb.py
@@ -9,6 +9,13 @@
 # Distributed under terms of the MIT license.
 
 import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+  except RuntimeError as e:
+    print(e)
 import numpy as np
 import scipy.misc
 
